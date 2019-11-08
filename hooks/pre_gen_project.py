1a6cb5ec8a75c32296d431179236683e15249e03
Axel Dahlberg <axel.dahlberg12@gmail.com>
2019-11-08 12:11:07 +0100

    The correct license is now applied and added checks for names

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,15 +1,22 @@
-import os
+snippet_name = "{{ cookiecutter.snippet_name }}"
+project_name = "{{ cookiecutter.project_name }}"
+project_slug = "{{ cookiecutter.project_slug }}"
+project_folder = "{{ cookiecutter.project_folder }}"
 
 
-def apply_license(license):
-    license_file = get_license_file(license)
+def assert_snippet_name(snippet_name):
+    if "netsquid" in snippet_name.lower():
+        raise ValueError("snippet_name should not contain 'NetSquid'")
 
 
-def get_license_file(license):
-    pass
+def assert_name(variable_name, variable, should_start_with):
+    if not variable.startswith(should_start_with):
+        raise ValueError(f"{variable_name} should start with '{should_start_with}', not {variable}")
 
 
-license = "{{ cookiecutter.license }}"
-print("Running before")
-print(f"License is {license}")
-print(f"cwd = {os.getcwd()}")
+assert_snippet_name(snippet_name)
+variable_names = [f"project_{name}" for name in ["name", "slug", "folder"]]
+names = [project_name, project_slug, project_folder]
+should_start_withs = ["NetSquid-", "netsquid-", "netsquid_"]
+for vname, v, sswith in zip(variable_names, names, should_start_withs):
+    assert_name(vname, v, sswith)
